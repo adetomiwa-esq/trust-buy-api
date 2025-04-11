2e793e382ffb8f52cc739ab771a577daa9c3c0ae
Akinola Kehinde <khennyakin@gmail.com>
2025-04-11 11:20:20 +0100

    feat: added feature to fetch all products and single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,3 +10,31 @@ export const newProduct = async (req, res, next) => {
     console.log(error);
   }
 };
+
+// fetch one product
+export const getOneProduct = async (req, res, next) => {
+  try {
+    if (!req.params.slug) {
+      const err = new Error(
+        `Product called ${req.params.slug} cannot be found`
+      );
+      err.status = 404;
+      return next(err);
+    }
+    const product = await Product.findOne({ slug: req.params.slug });
+
+    res.status(200).json(product);
+  } catch (error) {}
+};
+
+// fetch all products
+
+export const getProducts = async (req, res, next) => {
+  try {
+    const product = await Product.find();
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { newProduct } from "../controllers/productController.js";
+import {
+  newProduct,
+  getOneProduct,
+  getProducts,
+} from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.route("/new").post(newProduct);
+productRouter.route("/:slug").get(getOneProduct);
+productRouter.route("/").get(getProducts);
 
 export default productRouter;
