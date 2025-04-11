3bea28c20589f42583531899d205fb086ffa0748
Akinola Kehinde <khennyakin@gmail.com>
2025-04-11 12:53:47 +0100

    feat: added feature to delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 import Product from "../models/productModel.js";
 
-// create new product
+// desc          create new product
+// End point     /api/product/new
 
 export const newProduct = async (req, res, next) => {
   try {
@@ -11,7 +12,8 @@ export const newProduct = async (req, res, next) => {
   }
 };
 
-// fetch one product
+// desc             fetch one product
+// Endpoint         /api/product/:slug
 export const getOneProduct = async (req, res, next) => {
   try {
     if (!req.params.slug) {
@@ -27,13 +29,36 @@ export const getOneProduct = async (req, res, next) => {
   } catch (error) {}
 };
 
-// fetch all products
+// desc           fetch all products
+// End point      /api/product
 
 export const getProducts = async (req, res, next) => {
   try {
     const product = await Product.find();
 
     res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// desc             delete product
+// Endpoint         /api/product/:slug
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.slug });
+
+    if (!product) {
+      const err = new Error(
+        `Product called ${req.params.slug} cannot be found`
+      );
+      err.status = 404;
+      return next(err);
+    }
+
+    await Product.findOneAndDelete({ slug: req.params.slug });
+
+    res.status(200).json({ msg: `${req.params.slug} deleted successfully` });
   } catch (error) {
     console.log(error);
   }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,13 @@ import {
   newProduct,
   getOneProduct,
   getProducts,
+  deleteProduct,
 } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.route("/new").post(newProduct);
-productRouter.route("/:slug").get(getOneProduct);
+productRouter.route("/:slug").get(getOneProduct).delete(deleteProduct);
 productRouter.route("/").get(getProducts);
 
 export default productRouter;
